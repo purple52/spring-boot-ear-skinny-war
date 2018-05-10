914394f473a7f99f5cd9a92506d8e9d8d26d93f8
Daniel Cruver <ddcruver@gmail.com>
2018-05-10 13:30:05 -0400

    Add platform example where platform ear contains (almost) all jars.  Skinny EAR (deploying /web1 and /web2) and Skinny WAR (deploying /skinny-web) are light and depend on the Platform EAR.

---
+++ skinny-war/src/main/java/org/example/app/skinnywar/config/ExampleConfig.java
@@ -0,0 +1,9 @@
+package org.example.app.skinnywar.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.app.skinnywar.controller")
+public class ExampleConfig {
+}
